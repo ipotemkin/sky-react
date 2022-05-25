4355fae1a591e541ff73301bbe42d82267c69f6d
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-05-25 19:12:45 +0300

    updated App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Book from './Book'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Book name="JS for beginners" year="2018" price="1000" />
+      <Book name="React for beginners" year="2019" price="1200" />
+      <Book name="Vue for beginners" year="2021" price="1500" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
