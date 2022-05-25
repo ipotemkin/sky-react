727fe2f21ee2a41debd040f0e386d3073fceb51b
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-05-25 19:19:38 +0300

    added Hello from React, React, Vue JS to App

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Book from './Book'
 function App() {
   return (
     <div className="App">
+      <h1 id="hello" className="class1">
+        Hello from React
+      </h1>
       <Book name="JS for beginners" year="2018" price="1000" />
       <Book name="React for beginners" year="2019" price="1200" />
       <Book name="Vue for beginners" year="2021" price="1500" />
+      <Book name="React" year="2020" price="1200" />
+      <Book name="Vue JS" year="2019" price="1100" />
     </div>
   )
 }
