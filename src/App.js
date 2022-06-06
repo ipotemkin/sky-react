0755a5755a90e526faa6be3453e349eef5048159
Igor Potemkin <igorpotemkin@MacBook-Air-Igor.local>
2022-06-06 23:39:10 +0300

    added input-number

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css'
-// import Book from './Book'
 import MinMax from './components/MinMax'
 
 function App() {
   return (
     <div className="App">
-      <MinMax min={5} max={10}/>
-      {/* <h1 id="hello" className="class1">
-        Hello from React
-      </h1>
-      <Book name="JS for beginners" year="2018" price="1000" />
-      <Book name="React for beginners" year="2019" price="1200" />
-      <Book name="Vue for beginners" year="2021" price="1500" />
-      <Book name="React" year="2020" price="1200" />
-      <Book name="Vue JS" year="2019" price="1100" /> */}
+      <MinMax min={5} max={20}/>
     </div>
   )
 }
--- src/components/MinMax.js
+++ src/components/MinMax.js
@@ -9,8 +9,8 @@ class MinMax extends React.Component {
   }
   
   incr = () => {
-    if (this.state.curCount < this.max)
-      this.setState((prevState) => ({curCount: prevState.curCount + 1}))
+    if (this.state.curCount < this.max) 
+    this.setState((prevState) => ({curCount: prevState.curCount + 1}))
   }
 
   decr = () => {
@@ -18,14 +18,35 @@ class MinMax extends React.Component {
       this.setState((prevState) => ({curCount: prevState.curCount - 1}))
   }
 
+  validate = (value) => {
+    if (!+value) return this.min
+    if (+value < this.min) return this.min
+    if (+value > this.max) return this.max
+    return +value
+  }
+  
+  onChangeCount = (event) => {
+    const { target } = event
+    this.setState(() => ({ curCount: this.validate(target.value) }))
+  }
+
   render() {
     const { curCount} = this.state
     
     return (
       <div>
-        <h2>Counter</h2>
+        <h2>Counter 1</h2>
         <button className="btn" type="button" onClick={this.decr}>-</button>
         <span> { curCount } </span>
+        <button className="btn" type="button" onClick={this.incr}>+</button>
+        
+        <h2>Counter 2</h2>
+        <button className="btn" type="button" onClick={this.decr}>-</button>
+        <input type="text"
+          value={ curCount }
+          onChange={this.onChangeCount}
+          className="input-number"
+        />
         <button className="btn" type="button" onClick={this.incr}>+</button>
       </div>
     )
