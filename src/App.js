7a57508a5f785f10af28bd418ce78b056ea2052c
Igor Potemkin <ipotemkin@rambler.ru>
2022-06-07 17:44:14 +0300

    added MinMaxFn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css'
-import PomodoroTracker from './components/PomodoroTracker'
-import Clock from './components/Clock'
-// import MinMax from './components/MinMax'
-import ReacTimer from './components/ReacTimer'
+// import PomodoroTracker from './components/PomodoroTracker'
+// import Clock from './components/Clock'
+import MinMaxFn from './components/MinMaxFn'
+// import ReacTimer from './components/ReacTimer'
 
 function App() {
   return (
     <div className="App">
-      {/* <MinMax min={5} max={20}/> */}
-      <ReacTimer />
+      <MinMaxFn min={5} max={20}/>      
+      {/* <ReacTimer />
       <hr />
       <Clock />
       <hr />
-      <PomodoroTracker />
+      <PomodoroTracker /> */}
     </div>
   )
 }
