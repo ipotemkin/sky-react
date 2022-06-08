975a53d04f2813f6d0dc0f9fc09d53e54b132830
Igor Potemkin <ipotemkin@rambler.ru>
2022-06-08 18:50:17 +0300

    modified two squares

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -3,15 +3,16 @@ import { useState } from "react"
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */ 
 
-function Square({color, size=100, onClick = () => {}}) {
+function Square({color, size=100, onClick = () => {}, visible=true, cls=''}) {
   const style = {
     backgroundColor: color,
     width: size,
     height: size,
-    borderRadius: 10
+    borderRadius: 10,
+    display: visible ? 'block' : 'none'
   }
   
-  return <div className="square" style={style} onClick={onClick}/>
+  return <div className={`square ${cls}`} style={style} onClick={onClick}/>
 }
 
 export default function Squares() {
@@ -23,11 +24,24 @@ export default function Squares() {
 
   return (
     <div>
-      <h1>Task #2: Two squares</h1>
+      <h1>Task #2: Two squares with a Square component</h1>
       <div className="center">
-          <Square color='blue'/>
-          <Square color={status ? 'green' : 'red'} onClick={click}/>
+        <div className="container">
+            <Square color='blue' cls="el-1"/>
+            <Square color='green' cls="el-2" visible={status} onClick={click}/>
+            <Square color='red' cls="el-3" visible={!status} onClick={click}/>
+        </div>
       </div>
+
+      <h1>Task #2.1: Two squares with a pure div</h1>
+      <div className="center">
+        <div className="container">
+            <div className="square el-1" style={{backgroundColor: 'blue'}} onClick={click}/>
+            <div className="square el-2" style={{backgroundColor: 'green', display: status ? 'block' : 'none'}} onClick={click}/>
+            <div className="square el-3" style={{backgroundColor: 'red', display: !status ? 'block' : 'none'}} onClick={click}/>
+        </div>
+      </div>
+
   </div>
   )
 }
