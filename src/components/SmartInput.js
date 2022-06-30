553fb03c0aa95e27e6340f19fbdb96a43712ae70
Igor Potemkin <ipotemkin@rambler.ru>
2022-06-30 17:46:54 +0300

    formatted SmartInput code

--- src/components/SmartInput.js
+++ src/components/SmartInput.js
@@ -3,59 +3,58 @@ import { useState } from "react"
 import propTypes from 'prop-types'
 
 export default function LoginPass({ required, placeholder, fieldType, validator, defaultErrorText }) {
-    const [error, setError] = useState(false)
-    const [fieldValue, setFieldValue] = useState('')
-    const [errorText, setErrorText] = useState("Field is required")
+  const [error, setError] = useState(false)
+  const [fieldValue, setFieldValue] = useState('')
+  const [errorText, setErrorText] = useState(defaultErrorText)
 
-    const isError = (value) => {
-        const res = validator(value)
-        if (res) {
-            setErrorText(res)
-            return setError(true)
-        }
-        return false
+  const isError = (value) => {
+    const res = validator(value)
+    if (res) {
+      setErrorText(res)
+      return setError(true)
     }
+    return false
+  }
 
-    const checkInput = () => 
-    {        
-        if (required && fieldValue.length === 0) {
-            setErrorText(defaultErrorText)
-            return setError(true)
-        }
-        return isError(fieldValue)
+  const checkInput = () => {        
+    if (required && fieldValue.length === 0) {
+      setErrorText(defaultErrorText)
+      return setError(true)
     }
+    return isError(fieldValue)
+  }
     
-    const handleChange = (event) => {
-        setError(false)
-        setFieldValue(event.target.value)
-    }
+  const handleChange = (event) => {
+    setError(false)
+    setFieldValue(event.target.value)
+  }
 
-    return (
-        <div>
-            <input
-                type={fieldType}
-                value={fieldValue}
-                placeholder={placeholder}
-                onBlur={checkInput}
-                onChange={handleChange}
-            />
-            <p style={{color: 'red'}}>{ error && errorText }</p>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        type={fieldType}
+        value={fieldValue}
+        placeholder={placeholder}
+        onBlur={checkInput}
+        onChange={handleChange}
+      />
+      <p style={{color: 'red'}}>{ error && errorText }</p>
+  </div>
+  )
 }
 
 LoginPass.propTypes = {
-    required: propTypes.bool,
-    placeholder: propTypes.string,
-    fieldType: propTypes.string,
-    validator: propTypes.func,
-    defaultErrorText: propTypes.string
+  required: propTypes.bool,
+  placeholder: propTypes.string,
+  fieldType: propTypes.string,
+  validator: propTypes.func,
+  defaultErrorText: propTypes.string
 }
 
 LoginPass.defaultProps = {
-    required: false,
-    placeholder: '',
-    fieldType: 'text',
-    validator: () => false,
-    defaultErrorText: "Field is required"
+  required: false,
+  placeholder: '',
+  fieldType: 'text',
+  validator: () => false,
+  defaultErrorText: "Field is required"
 }
